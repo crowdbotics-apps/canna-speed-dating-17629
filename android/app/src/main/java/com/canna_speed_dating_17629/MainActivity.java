97e504963093220ec7c03250c63b0897a2f39612
Crowdbotics <team@crowdbotics.com>
2020-05-31 22:04:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/canna_speed_dating_17629/MainActivity.java
@@ -0,0 +1,15 @@
+package com.canna_speed_dating_17629;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "canna_speed_dating_17629";
+    }
+}
